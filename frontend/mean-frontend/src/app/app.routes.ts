99320eb90f12c75c4c9037adfbe52132fee58c35
Liantsiky <malalatsiky11@gmail.com>
2026-02-23 22:43:50 +0300

    test layout

--- frontend/mean-frontend/src/app/app.routes.ts
+++ frontend/mean-frontend/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { EtageFormComponent } from './features/etage/etage-form/etage-form.component';
+import { AdminLayoutComponent } from './layout/admin/admin-layout/admin-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : 'etage/creer',
+        component : EtageFormComponent
+    },
+    {
+        path: 'admin',
+        component: AdminLayoutComponent,
+        children: [
+
+        {
+            path: 'dashboard',
+            loadComponent: () =>
+            import('./features/admin/dashboard/dashboard.component')
+            .then(m => m.DashboardComponent)
+        },
+        {
+            path: 'etage/create',
+            loadComponent: () =>
+            import('./features/admin/etage/etage-form/etage-form.component')
+            .then(m => m.EtageFormComponent)
+        }
+
+    ]
+  }
+];
