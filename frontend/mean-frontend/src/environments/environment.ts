1e00a777d34c8ee86638ce5b3f5105570cd4407a
Ny Aina Ratolojanahary <118933919+NyAinaRatolojanahary@users.noreply.github.com>
2026-03-11 19:46:08 +0300

    Update apiUrl in environment configuration

--- frontend/mean-frontend/src/environments/environment.ts
+++ frontend/mean-frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     //apiUrl: 'http://localhost:3000/api',
-  apiUrl: 'https://m1p13mean-liantsiky-nyaina.onrender.com/api/'
+  apiUrl: 'https://m1p13mean-liantsiky-nyaina.onrender.com/api'
 };
